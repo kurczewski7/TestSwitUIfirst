467e3937b33fbd61fc0b6cc0272ee1343e3148e7
Kurczewski Slawomir <slawomir.kurczewski@gmail.com>
2019-11-24 22:13:14 +0100

    Initial Commit

---
+++ TestSwitUIfirst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSwitUIfirst
+//
+//  Created by Slawek Kurczewski on 24/11/2019.
+//  Copyright © 2019 Slawomir Kurczewski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
